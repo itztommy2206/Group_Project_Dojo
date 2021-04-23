6a439d67fe52b11c5666eef89d491673017e3b00
Tommy Mac <mact@uw.edu>
2021-04-23 12:00:06 -0700

    added some models.py stuff in the file

--- group_app/models.py
+++ group_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length = 255)
+    last_name = models.CharField(max_length = 255)
+    email = models.CharField(max_length = 255)
+    password = models.TextField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
